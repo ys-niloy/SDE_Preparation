72936497c8df292e2703701cd0a9b57217353657
YS Niloy <115401989+ys-niloy@users.noreply.github.com>
2023-08-28 23:10:50 +0600

    Create list.cpp

---
+++ STL/list.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    list<int> ls;
+
+    ls.push_back(23);           // 23
+    ls.emplace_back(24);        // 23, 24
+    ls.push_front(21);          // 21, 23, 24
+    
+
+
+    // rest of the functions are like vector
+}
